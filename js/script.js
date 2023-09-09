2e4f3a64bcc90958ceef67dd5d204dbb33fe0756
liefesutliff <123669909+liefesutliff@users.noreply.github.com>
2023-09-09 08:21:27 -0500

    Added Responsive Design To Menu And added toggle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const toggleButton = document.getElementById('toggleButton');
-  const navList = document.getElementsByClassName('linksList');
+  const navList = document.querySelector('.linksList');
 
   toggleButton.addEventListener('click', () => {
     navList.classList.toggle('active');
